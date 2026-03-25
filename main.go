6205709147872ccc56f431fecbbc297f4f013536
deepestvalue <deepestvalue@proton.me>
2026-03-24 20:42:27 -0700

    Problem two valid palindrome

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	p1 "leetcode_go/problems/problem1_two_sum"
+	p2 "leetcode_go/problems/problem2_valid_palindrome"
 )
 
 func main() {
+	// Problem 1
+	fmt.Println("\nProblem 1 Two Sum")
 	res := p1.TwoSum([]int{2, 7, 11, 15}, 9)
 	fmt.Println(res)
 
@@ -14,4 +17,11 @@ func main() {
 
 	res = p1.TwoSumSol3([]int{2, 7, 11, 15}, 9)
 	fmt.Println(res)
+
+	// Problem 2
+	fmt.Println("\nProblem 2 Valid Palindrome")
+	fmt.Println(p2.IsPalindrome("hi"))
+	fmt.Println(p2.IsPalindrome("A man, a plan, a canal: Panama"))
+	fmt.Println(p2.IsPalindrome("race a car"))
+	fmt.Println(p2.IsPalindrome("0P"))
 }
---
+++ problems/problem2_valid_palindrome/problem2_valid_palindrome.go
@@ -0,0 +1,27 @@
+package problem2validpalindrome
+
+import "unicode"
+
+func IsPalindrome(s string) bool {
+	left, right := 0, len(s) - 1
+
+	for left < right {
+		for left < right && !(unicode.IsLetter(rune(s[left])) || unicode.IsDigit(rune(s[left]))) {
+			left++
+		}
+
+		for left < right && !(unicode.IsLetter(rune(s[right])) || unicode.IsDigit(rune(s[right]))) {
+			right--
+		}
+
+		if unicode.ToLower(rune(s[left])) != unicode.ToLower(rune(s[right])) {
+			return false
+		}
+
+		left++
+		right--
+
+	}
+
+	return true
+}
